d0bd215e4b60b137c4e0fba5d4e06995e9f1ebd2
Mikhail Noskov <nm@perm-tools.ru>
2017-09-01 05:30:22 +0000

    tests

--- tests/TranslatorTest.php
+++ tests/TranslatorTest.php
@@ -4,30 +4,70 @@
 
 use PHPUnit\Framework\TestCase;
 
+class Parser extends \Amcms\Quad\Quad {
+    public function getField($name) {
+        return 'VALUE OF FIELD "' . $name . '"';
+    }
+
+    public function getConfig($name) {
+        return 'VALUE OF SETTING "' . $name . '"';
+    }
+
+    public function makeUrl($id) {
+        return 'LINK FOR "' . $id . '"';
+    }
+}
+
+
 class TranslatorTest extends TestCase {
 
-    const SNIPPET     = 1;
-    const CHUNK       = 2;
-    const PLACEHOLDER = 3;
-    const DOCFIELD    = 4;
-    const SETTING     = 5;
-
-    protected $translator;
+    protected $parser;
 
     public function setUp() {
-        $this->translator = new \Amcms\Quad\Translator;
+        $this->parser = new Parser([
+            'cache'     => false,
+            'templates' => __DIR__ . '/templates',
+        ]);
+
+        $this->parser->registerSnippet('getCacheMode', function($parameters, $cached) {
+            return $cached ? 'cached' : 'uncached';
+        });
+
+        $this->parser->registerSnippet('getParam', function($parameters) {
+            if ($parameters['what'] == 'name') {
+                return key($parameters);
+            } else {
+                return reset($parameters);
+            }
+        });
+
+        $this->parser->registerSnippet('sum', function($parameters) {
+            return array_reduce($parameters, function($sum, $arg) {
+                return $sum + intval($arg);
+            });
+        });
+
+        $this->parser->registerFilter('add', function($input, $parameter) {
+            return intval($input) + intval($parameter);
+        });
+
+        $this->parser->setPlaceholder('a', 1);
+        $this->parser->setPlaceholder('b', 2);
     }
 
     public function providerParse() {
         return [
-            ['[[snippet]]', 'cached snippet'],
-            ['[!snippet!]', 'uncached snippet'],
-            ['[[getParamName? &parameter=`1`]]', 'parameter'],
-            ['[[sum? &param1=`1` &param2=`2`]]', 3],
-            ['[!sum? &param1=`1` &param2=`2` &param3=`3`!]', 6],
-            ['[[sum? &param1=`1` &param2=`[+param2+]`]]', 3],
-            ['[[sum? &param1=`1` &param2=`[[sum? &param1=`1` &param2=`2`]]`]]', 4],
-            ['[+param1+] + [+param2+]', '1 + 2'],
+            ['@CODE: [[getCacheMode]]', 'cached'],
+            ['@CODE: [!getCacheMode!]', 'uncached'],
+            ['@CODE: [[getParam? &parameter=`1` &what=`name`]]', 'parameter'],
+            ['@CODE: [[getParam? &parameter=`1` &what=`value`]]', '1'],
+            ['@CODE: [[sum? &a=`1` &b=`2`]]', 3],
+            ['@CODE: [!sum? &a=`1` &b=`2` &c=`3`!]', 6],
+            ['@CODE: [[sum? &a=`1` &b=`[+b+]`]]', 3],
+            ['@CODE: [[sum? &a=`1` &b=`[[sum? &a=`1` &b=`2`]]`]]', 4],
+            ['@CODE: [+a+] + [+b+]', '1 + 2'],
+            ['@CODE: [+a:add=`3`+]', 4],
+            ['@CODE: [+a:add=`[+b+]`+]', 3],
         ];
     }
 
@@ -36,64 +76,10 @@ class TranslatorTest extends TestCase {
      */
     public function testParse($input, $result) {
         echo "\n" . $input . "\n";
-        $output = $this->translator->parse($input);
-        echo $output . "\n";
-        $output = preg_replace('/^<\?php echo (.+); \?>$/', 'return implode([$1]);', $output);
-        $output = eval($output);
+        $output = $this->parser->renderTemplate($input);
         echo $output . "\n";
 
         $this->assertEquals($output, $result);
-    }
-
-    private function call(...$args) {
-        switch ($args[0]) {
-            case self::SNIPPET: {
-                $name = "call" . ucfirst($args[1]);
-                if (count($args) == 4) {
-                    return $this->$name($args[2], $args[3]);
-                } else {
-                    return $this->$name([], $args[2]);
-                }
-            }
-
-            case self::CHUNK: {
-                $name = "getChunk" . ucfirst($args[1]);
-                return $this->$name([], $args[2]);
-            }
-
-            case self::SETTING:
-            case self::DOCFIELD:
-            case self::PLACEHOLDER: {
-                $name = "getField" . ucfirst($args[1]);
-                return $this->$name();
-            }
-        }
-
-        return 'unknown call';
-    }
-
-    private function getFieldParam1() {
-        return 1;
-    }
-
-    private function getFieldParam2() {
-        return 2;
-    }
-
-    private function callSnippet($params, $cached) {
-        return ($cached ? 'cached' : 'uncached') . ' snippet';
-    }
-
-    private function callGetParamName($params, $cached) {
-        $this->assertEquals(1, count($params));
-        return key($params);
-    }
-
-    private function callSum($params, $cached) {
-        $this->assertGreaterThan(0, count($params));
-        return array_reduce($params, function($result, $item) {
-            return $result += $item;;
-        });
     }
 
 }
